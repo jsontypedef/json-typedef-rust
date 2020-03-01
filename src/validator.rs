7fcf1abc6961c25362aee5e81e87ed06effe3d16
Ulysse Carion <ulyssecarion@gmail.com>
2020-02-29 18:24:03 -0800

    Implement the type form

--- src/validator.rs
+++ src/validator.rs
@@ -1,4 +1,6 @@
+use crate::form;
 use crate::schema::Schema;
+use chrono::DateTime;
 use serde_json::Value;
 
 #[derive(Debug)]
@@ -7,13 +9,204 @@ pub struct Validator {
     pub max_errors: Option<usize>,
 }
 
+#[derive(Debug)]
 pub struct ValidationError {
     pub instance_path: Vec<String>,
     pub schema_path: Vec<String>,
 }
 
+#[derive(Debug)]
+pub enum ValidateError {
+    MaxDepthExceeded,
+}
+
 impl Validator {
-    pub fn validate(schema: Schema, instance: Value) -> Vec<ValidationError> {
-        vec![]
+    pub fn validate(
+        &self,
+        schema: &Schema,
+        instance: &Value,
+    ) -> Result<Vec<ValidationError>, ValidateError> {
+        let mut vm = Vm {
+            max_depth: self.max_depth,
+            max_errors: self.max_errors,
+            instance_tokens: vec![],
+            schema_tokens: vec![vec![]],
+            errors: vec![],
+        };
+
+        match vm.validate(schema, schema, instance) {
+            Ok(()) | Err(VmValidateError::MaxErrorsReached) => Ok(vm.errors),
+            Err(VmValidateError::MaxDepthExceeded) => Err(ValidateError::MaxDepthExceeded),
+        }
+    }
+}
+
+struct Vm {
+    pub max_depth: Option<usize>,
+    pub max_errors: Option<usize>,
+    pub instance_tokens: Vec<String>,
+    pub schema_tokens: Vec<Vec<String>>,
+    pub errors: Vec<ValidationError>,
+}
+
+enum VmValidateError {
+    MaxErrorsReached,
+    MaxDepthExceeded,
+}
+
+impl Vm {
+    pub fn validate(
+        &mut self,
+        root: &Schema,
+        schema: &Schema,
+        instance: &Value,
+    ) -> Result<(), VmValidateError> {
+        match &schema.form {
+            form::Form::Empty => {}
+            form::Form::Type(form::Type {
+                nullable,
+                type_value,
+            }) => {
+                if !*nullable || !instance.is_null() {
+                    self.push_schema_token("type");
+
+                    match type_value {
+                        form::TypeValue::Boolean => {
+                            if !instance.is_boolean() {
+                                self.push_error()?;
+                            }
+                        }
+                        form::TypeValue::Float32 | form::TypeValue::Float64 => {
+                            if !instance.is_f64() && !instance.is_i64() {
+                                self.push_error()?;
+                            }
+                        }
+                        form::TypeValue::Int8 => self.validate_int(instance, -128.0, 127.0)?,
+                        form::TypeValue::Uint8 => self.validate_int(instance, 0.0, 255.0)?,
+                        form::TypeValue::Int16 => self.validate_int(instance, -32768.0, 32767.0)?,
+                        form::TypeValue::Uint16 => self.validate_int(instance, 0.0, 65535.0)?,
+                        form::TypeValue::Int32 => {
+                            self.validate_int(instance, -2147483648.0, 2147483647.0)?
+                        }
+                        form::TypeValue::Uint32 => {
+                            self.validate_int(instance, 0.0, 4294967295.0)?
+                        }
+                        form::TypeValue::String => {
+                            if !instance.is_string() {
+                                self.push_error()?;
+                            }
+                        }
+                        form::TypeValue::Timestamp => {
+                            if let Some(s) = instance.as_str() {
+                                if DateTime::parse_from_rfc3339(s).is_err() {
+                                    self.push_error()?;
+                                }
+                            } else {
+                                self.push_error()?;
+                            }
+                        }
+                    };
+
+                    self.pop_schema_token();
+                }
+            }
+            _ => unimplemented!(),
+        };
+
+        Ok(())
+    }
+
+    fn validate_int(
+        &mut self,
+        instance: &Value,
+        min: f64,
+        max: f64,
+    ) -> Result<(), VmValidateError> {
+        if let Some(val) = instance.as_f64() {
+            if val.fract() != 0.0 || val < min || val > max {
+                self.push_error()
+            } else {
+                Ok(())
+            }
+        } else {
+            self.push_error()
+        }
+    }
+
+    fn push_error(&mut self) -> Result<(), VmValidateError> {
+        self.errors.push(ValidationError {
+            instance_path: self.instance_tokens.clone(),
+            schema_path: self.schema_tokens.last().unwrap().clone(),
+        });
+
+        Ok(())
+    }
+
+    fn push_schema_token(&mut self, token: &str) {
+        self.schema_tokens
+            .last_mut()
+            .unwrap()
+            .push(token.to_owned());
+    }
+
+    fn pop_schema_token(&mut self) {
+        self.schema_tokens.last_mut().unwrap().pop();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+    use std::collections::HashMap;
+    use std::convert::TryInto;
+
+    #[test]
+    fn spec_validation_suite() {
+        #[derive(Serialize, Deserialize)]
+        struct TestCase {
+            schema: crate::serde::Schema,
+            instance: Value,
+            errors: Vec<TestCaseError>,
+        }
+
+        #[derive(Serialize, Deserialize, PartialEq, Debug)]
+        #[serde(rename_all = "camelCase")]
+        struct TestCaseError {
+            instance_path: Vec<String>,
+            schema_path: Vec<String>,
+        }
+
+        let test_cases: HashMap<String, TestCase> =
+            serde_json::from_str(include_str!("../json-typedef-spec/tests/validation.json"))
+                .unwrap();
+
+        for (name, test_case) in test_cases {
+            let schema = test_case
+                .schema
+                .try_into()
+                .expect(&format!("parsing schema: {}", name));
+
+            let validator = Validator {
+                max_depth: None,
+                max_errors: None,
+            };
+
+            let errors: Vec<_> = validator
+                .validate(&schema, &test_case.instance)
+                .expect(&format!("validating: {}", name))
+                .into_iter()
+                .map(|err| TestCaseError {
+                    instance_path: err.instance_path,
+                    schema_path: err.schema_path,
+                })
+                .collect();
+
+            assert_eq!(
+                test_case.errors, errors,
+                "wrong set of errors returned for test case: {}",
+                name
+            );
+        }
     }
 }
